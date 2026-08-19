2b88f2b733e2a88c416f719be527e62c57aed2da
Shailesh Kumar <kumarshailesh4914@gmail.com>
2026-08-19 17:48:44 +0000

    DataTypes

---
+++ DataType.js
@@ -0,0 +1,35 @@
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// console.log(a + b +c);
+
+//alert( 3+3)// we are using node.js not browser
+
+//console.log(3+3); console.log("shailesh kumar ") // code sahi hai lekin readeable nahi hai  
+//ye achhi practice nahi hai ok 
+ //code readeability ,future proof bhi hona chahiye 
+
+
+console.log( " shailesh")
+let name = " shailesh "
+let gae = 18;
+let isLoggedIn = false ;
+let state ;
+
+
+
+// number => 2 to power 53
+//string " "
+// boolean -- ture /false 
+// null => standalone value hai 
+// undefined => 
+// symbol=> unique
+
+
+//object
+
+
+console.log(typeof "shailesh ");
+console.log( typeof null);
